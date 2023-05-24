d0562f4c961437fc033f2c5da55b5276c2850f1b
MarieBR899 <133834872+MarieBR899@users.noreply.github.com>
2023-05-24 17:08:38 -0600

    Update MainPage.py

--- MainPage.py
+++ MainPage.py
@@ -19,16 +19,20 @@ así como el método  de cuasi Newton o Broyden.
 Este método requiere $n$ evaluaciones  funcionales por iteración  ya que remplaza la matriz jacobiana  con una matriz de aproximación 
 que se actualiza en cada iteración y también disminuye el número de  calculos aritméticos a $O(n^2)$. Pertenece a una clase de tecnicas que 
 reemplazan a la matriz jacobiana con una matriz de aproximación  que se actualiza con cada iteración. 
-Su desventaja radica en que se pierde  la convergencia cuadrática de Newton, al ser sustituida por una convergencia denominada  superlineal , la cual implica que:
+Su desventaja radica en que se pierde  la convergencia cuadrática de Newton, al ser sustituida por una convergencia denominada superlineal, 
+la cual implica que:  
 
+ \lim_{x\to\infty}  (\frac{||X^{(x+1)}-X||}{||X^{(i)}-X||}=0
 '''
 
 '''
-El método de Broyden condiste en que a partir de 2 aproximaciones iniciales
-$X^(0)$ y $X^(1)$ y la solucion de $x$ de $F(x)=0
-se calcula de $X^(2)$ en afelante von el metofo fe Broyden.
-La primera uteracion de calcula por método fe Newton 
-o si es difícil determinar se calcula por $J(x^(0))$ se utilizan las ecuaciones de diferencias para aproximar las derivadas parciales.
+El método de Broyden condiste en que a partir de 2 aproximaciones iniciales:
+
+
+$X^{(0)}$ y $ X^{(1)}$ y la solucion de $X$ de $F(x)=0
+
+se calcula de $X^{(2)}$ en afelante von el método de Broyden.
+La primera iteracion se calcula por método de Newton, o si es difícil determinar se calcula por $J(x^(0))$ se utilizan las ecuaciones de diferencias para aproximar las derivadas parciales.
 
 Antes de iniciar con el metodo de Broyden es importante recordar que el metodo de la secante multivariable implica la siguiente fórmula para sustituir el cálculo de la derivadas
 
