67f905fd0f1f9d1168c79cb4f3ff7c2aeb6f5504
MarieBR899 <133834872+MarieBR899@users.noreply.github.com>
2023-05-28 15:40:31 -0600

    Update MainPage.py

--- MainPage.py
+++ MainPage.py
@@ -135,7 +135,7 @@ Se puede reescribir como: $ (A^{(k)})^{-1} = (A+XY^{(t)})^{-1} $  lo que se ajus
 """
 
 st.latex(r"""
-    (A^{(k})^{-1} = (A^{(k-1})^{-1} = \frac{[\Delta X^{(k)}-(A^{(k-1)})^{-1} \Delta F^{(k)}]}{||\Delta X^{(k)}||_2^{2}} , Y = \Delta X^{k}
+    (A^{(k})^{-1} = (A^{(k-1})^{-1} + \frac{[\Delta X^{(k)}-(A^{(k-1)})^{-1} \Delta F^{(k)}] (\Delta X^{(k)})^{t}(A^{(k-1)})^{-1}}{||\Delta X^{(k)}||_2^{2}} , Y = \Delta X^{k}
 
     """)
 
