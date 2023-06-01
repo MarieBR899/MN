0e44f075a1918694262e0862d3d5e8be1fe68c6a
MarieBR899 <133834872+MarieBR899@users.noreply.github.com>
2023-06-01 06:48:02 -0600

    Update Splines.py

--- pages/Splines.py
+++ pages/Splines.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+
 tab1, tab2, tab3 = st.tabs(["Definiciones","Ejemplos","Aplicaciones"])
 with tab1:
     st.title(":blue[**Splines cubicos**]")
