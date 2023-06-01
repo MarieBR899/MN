88ef3a525b85479fe270545787eef61559ed6b4d
MarieBR899 <133834872+MarieBR899@users.noreply.github.com>
2023-06-01 02:42:52 -0600

    Update Lagrange.py

--- pages/Lagrange.py
+++ pages/Lagrange.py
@@ -119,6 +119,7 @@ P_n(x)f(x_0)L_0(x) + f(x_1)L_1(x)+...+f(x_n)L_n(x)=\displaystyle\sum_{k=0}^{n}x_
 Dónde $L_k(x)$ está dado por la ecuación del teorema anterior
 La técnica usada para construir $P(x)$ esto de interpolación hola que se empleaba para construir las tablas trigonométricas con logarítmicas. 
 """
+
 with tab2:
 
 """
